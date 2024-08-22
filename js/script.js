14ba7650c409b612a690d4104f933f7e8fcf9266
Romina <romina-bustelo@hotmail.com>
2024-08-22 20:13:12 -0300

    Inclusión de Fetch

--- js/script.js
+++ js/script.js
@@ -19,3 +19,13 @@ function showData(dataArray) {
 }
 
 // Escribe el código necesario para realizar el fetch al archivo con los datos y mostrar los estudiantes con la función showData
+
+// Solicitar los datos del archivo JSON y mostrar los estudiantes
+fetch(DATA_URL)
+  .then(response => response.json()) // Convertir la respuesta en JSON
+  .then(data => {
+    showData(data.students); // Mostrar los estudiantes
+  })
+  .catch(error => {
+    console.error('Error fetching the data:', error); // Mostrar error en caso de fallo
+  });
